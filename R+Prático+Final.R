21352721a2151b2d99fa203a2a7690517823c555
Ana Julia Castelo Branco Fernandes <anajuliacbfernandes@gmail.com>
2024-10-01 11:15:44 -0300

    Update R+Prático+Final.R

--- R+Prático+Final.R
+++ R+Prático+Final.R
@@ -19,7 +19,7 @@ notas <- function(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t) {
     cat(" A mediana das notas é:", nota_mediana, "|")
   }
   
-  #Para 50% menores notas e maiores notas - média
+  #Para 50% menores e maiores notas - média
   menores_notas_media <- mean(notas_ordenadas[0:(tamanho_lista/2)])
   maiores_notas_media <- mean(notas_ordenadas[((tamanho_lista/2)+1):tamanho_lista])
   cat(" A média das 50% menores notas é:", menores_notas_media, "|")
